ab07557597b585e389a2e5712f3b0cb924b90455
chuanyao-G <1647298067@qq.com>
2019-02-14 08:55:32 +0800

    library

---
+++ banner插件/tfboys/js/tfboys-.js
@@ -0,0 +1,20 @@
+﻿	var currentSwiper = new Swiper('.current-swiper',{
+		loop : true,
+	});
+	currentSwiper.$el.parent().find('.swiper-button-next').on('click',function(){//去除按钮阴影
+		currentSwiper.slideNext();
+	})
+	currentSwiper.$el.parent().find('.swiper-button-prev').on('click',function(){
+		currentSwiper.slidePrev();
+	})			
+	var leftSwiper = new Swiper('.left-swiper',{
+		loop : true,
+		simulateTouch : false,
+	});
+	var rightSwiper = new Swiper('.right-swiper',{
+		loop : true,
+		simulateTouch : false,
+	});			
+	currentSwiper.controller.control = [leftSwiper,rightSwiper];
+	
+	//https://www.css-js.com/ jspack压缩
